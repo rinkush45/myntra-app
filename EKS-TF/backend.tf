9c58473110c95da113ceaf73dae575269423e8f7
Rinku sharma <rinkusharma.official@yahoo.com>
2024-04-04 16:08:16 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myntraclone0" # Replace with your actual S3 bucket name
+    bucket = "myntra45" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
